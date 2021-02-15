360cbe4f3ae427b903e5eb2462b528e96c83b41e
henrique77 <henriquecaires@outlook.com>
2021-02-15 15:10:56 -0300

    Criado tela de listagem de evento

--- agenda/settings.py
+++ agenda/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'agenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
